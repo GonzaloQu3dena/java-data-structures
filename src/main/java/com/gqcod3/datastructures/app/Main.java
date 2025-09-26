29b75ffc57d33d30d3ed64776e84c113b0ea3d72
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2025-09-26 11:34:03 -0500

    feat(circular): add example for circular linked list.

--- src/main/java/com/gqcod3/datastructures/app/Main.java
+++ src/main/java/com/gqcod3/datastructures/app/Main.java
@@ -1,5 +1,6 @@
 package com.gqcod3.datastructures.app;
 
+import com.gqcod3.datastructures.lists.circular.CircularLinkedList;
 import com.gqcod3.datastructures.lists.doubly.DoublyLinkedList;
 import com.gqcod3.datastructures.lists.singly.SinglyLinkedList;
 
@@ -27,5 +28,16 @@ public class Main {
        doublyLinkedList.iterator('f');
        System.out.println("\nDoubly Linked List: backward iteration");
        doublyLinkedList.iterator('b');
+
+        /**
+        * Circular Linked List Example
+        */
+       CircularLinkedList<Double> circularLinkedList = new CircularLinkedList<Double>();
+       circularLinkedList.pushBack(1.5);
+       circularLinkedList.pushBack(10.6);
+       circularLinkedList.pushBack(2.8);
+
+       System.out.println("\nCircular Linked List:");
+       circularLinkedList.iterator(x -> System.out.print(x + " -> "));
     }
 }
