acf18355a6bcc3b19d270a25fb575dd873df0670
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2025-09-26 11:32:12 -0500

    feat(doubly): add example for doubly linked list.

--- src/main/java/com/gqcod3/datastructures/app/Main.java
+++ src/main/java/com/gqcod3/datastructures/app/Main.java
@@ -1,5 +1,6 @@
 package com.gqcod3.datastructures.app;
 
+import com.gqcod3.datastructures.lists.doubly.DoublyLinkedList;
 import com.gqcod3.datastructures.lists.singly.SinglyLinkedList;
 
 public class Main {
@@ -14,5 +15,17 @@ public class Main {
        linkedList.pushBack(3);
        System.out.println("\nSingly Linked List:");
        linkedList.iterator();
+
+       /**
+        * Doubly Linked List Example
+        */
+       DoublyLinkedList<String> doublyLinkedList = new DoublyLinkedList<String>();
+       doublyLinkedList.pushBack("Hello");
+       doublyLinkedList.pushBack("World");
+
+       System.out.println("\nDoubly Linked List: forward iteration");
+       doublyLinkedList.iterator('f');
+       System.out.println("\nDoubly Linked List: backward iteration");
+       doublyLinkedList.iterator('b');
     }
 }
