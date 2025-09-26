3bf709e7de97419c662b811a0cc93c34618ad42d
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2025-09-26 11:27:35 -0500

    feat(singly): add example for singly linked list.

--- src/main/java/com/gqcod3/datastructures/app/Main.java
+++ src/main/java/com/gqcod3/datastructures/app/Main.java
@@ -1,7 +1,18 @@
 package com.gqcod3.datastructures.app;
 
+import com.gqcod3.datastructures.lists.singly.SinglyLinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        /**
+        * Singly Linked List Example
+        */ 
+       SinglyLinkedList<Integer> linkedList = new SinglyLinkedList<Integer>();
+       linkedList.pushBack(1);
+       linkedList.pushBack(2);
+       linkedList.pushBack(3);
+       System.out.println("\nSingly Linked List:");
+       linkedList.iterator();
     }
 }
